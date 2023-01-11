eee2806ba5e58466365858cdfd4dacd0793566e5
Mokinder <mokindershekaar2006@gmail.com>
2023-01-11 12:11:36 +0530

    Revert "I removed the files"
    This reverts commit 99584ad4a7697ac6d60cd63c1bfc73cef2c974a1.

--- src/secondprog.java
+++
@@ -1,9 +0,0 @@
-class Secondprog{
-    public static void main(String args[]){
-        int a=4;
-        int b=7;
-        public void add(){
-            System.out.println("Addition is : "+(a+b));
-        }
-    }
-}
