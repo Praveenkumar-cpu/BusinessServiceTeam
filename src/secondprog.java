99584ad4a7697ac6d60cd63c1bfc73cef2c974a1
Mokinder <mokindershekaar2006@gmail.com>
2023-01-11 11:12:25 +0530

    mokibrh 1 change

---
+++ src/secondprog.java
@@ -0,0 +1,9 @@
+class Secondprog{
+    public static void main(String args[]){
+        int a=4;
+        int b=7;
+        public void add(){
+            System.out.println("Addition is : "+(a+b));
+        }
+    }
+}
